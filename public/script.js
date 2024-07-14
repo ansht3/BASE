8154c337dab6a1b5ec96c32bd385f2863c091483
mehta302 <mehta302@purdue.edu>
2024-07-13 21:42:44 -0400

    Firebase Initialization

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+async function pipeline() {
+  const prompt = "Hello";
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+pipeline();
+
