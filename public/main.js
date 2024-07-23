f7c3dfacbf594d9a2cb673bf7674f66321ef4a9b
mehta302 <mehta302@purdue.edu>
2024-07-23 19:38:24 -0400

    Main Routine

---
+++ public/main.js
@@ -0,0 +1,145 @@
+import * as d3 from "d3";
+
+class Main {
+  constructor() {}
+
+  // Naive Approach
+  static functionMap = {
+    addTag: ({selection, tag, attributes, styles, text}) => {
+      const element = d3.selectAll(selection).append(tag);
+
+      for (const attribute of attributes) {
+        element.attr(attribute, attributes[attribute]);
+      }
+
+      for (const style of styles) {
+        element.style(style, styles[style]);
+      }
+
+      if (text) {
+        element.text(text);
+      }
+    },
+
+    alterTag: ({selection, attributes, styles, text}) => {
+      const element = d3.selectAll(selection);
+
+      for (const attribute of attributes) {
+        element.attr(attribute, attributes[attribute]);
+      }
+
+      for (const style of styles) {
+        element.style(style, styles[style]);
+      }
+
+      if (text) {
+        element.text(text);
+      }
+    },
+
+    deleteTag: ({selection}) => {
+      d3.selectAll(selection).remove();
+    }
+  };
+
+  // Elementwise
+  static elementMap = {
+    addDiv: ({selection, styles, text, id, _class}) => {
+      const div = d3.selectAll(selection).append("div");
+
+      for (const style of styles) {
+        div.style(style, styles[style]);
+      }
+
+      if (text) {
+        div.text(text);
+      }
+
+      if (id) {
+        div.attr("id", id);
+      }
+
+      if (_class) {
+        div.attr("class", _class);
+      }
+    },
+  };
+
+  static functionDeclarations = [
+    {
+      name: "addTag",
+      parameters: {
+        type: "OBJECT",
+        description: "Add an HTML element based on appending to a CSS selector, tag name, attributes, inline styles, and text",
+        properties: {
+          selection: {
+            type: "STRING",
+            description: "CSS selector to append element to. For single selection use id eg '#name', group selection use class eg '.name' and for all tags use just name of tag eg 'div'"
+          },
+          tag: {
+            type: "STRING",
+            description: "Name of HTML tag to append eg div"
+          },
+          attributes: {
+            type: "OBJECT",
+            description: "A JSON object of a list of tag attributes and their specified values eg '{ id: \"main\" }'"
+          },
+          styles: {
+            type: "OBJECT",
+            description: "A JSON object of a list of tag styles and their specified values eg '{ color: \"red\" }'"
+          },
+          text: {
+            type: "STRING",
+            description: "The text to be inserted into the newly generated tag"
+          }
+        }
+      },
+      required: ["tag", "attributes", "styles", "text"]
+    },
+    {
+      name: "alterTag",
+      parameters: {
+        type: "OBJECT",
+        description: "Select HTML tag or tags based on a CSS selector, alter attributes, alter inline styles, and alter text",
+        properties: {
+          selection: {
+            type: "STRING",
+            description: "CSS selector to select elements to alter. For single selection use id eg '#name', group selection use class eg '.name' and for all tags use just name of tag eg 'div'"
+          },
+          attributes: {
+            type: "OBJECT",
+            description: "A JSON object of a list of tag attributes and their specified values eg '{ id: \"main\" }'"
+          },
+          styles: {
+            type: "OBJECT",
+            description: "A JSON object of a list of tag styles and their specified values eg '{ color: \"red\" }'"
+          },
+          text: {
+            type: "STRING",
+            description: "The text to replace the selected tag current text"
+          }
+        }
+      },
+      required: ["tag", "attributes", "styles", "text"]
+    },
+    {
+      name: "deleteTag",
+      parameters: {
+        type: "OBJECT",
+        description: "Select HTML tag or tags based on a CSS selector to delete",
+        properties: {
+          selection: {
+            type: "STRING",
+            description: "CSS selector to select elements to delete. For single selection use id eg '#name', group selection use class eg '.name' and for all tags use just name of tag eg 'div'"
+          }
+        }
+      }
+    }
+  ];
+
+  static noFunction() {
+    alert("Unable to Fulfill Request");
+  }
+}
+
+export default Main;
--- public/script.js
+++ public/script.js
@@ -1,14 +1,33 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import Main from "./main.js";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  tools: { functionDeclarations: Main.functionDeclarations },
+  toolConfig: {
+    functionCallingConfig: {
+      mode: "ANY"
+    }
+  }
+});
 
-async function pipeline() {
-  const prompt = "Hello";
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
+async function pipeline(prompt) {
+  const chat = model.startChat();
+  const result = await chat.sendMessage(prompt);
+  const calls = result.response.functionCalls();
+  console.log(calls);
+
+  if (calls.length > 0) {
+    for (const call of calls) {
+      Main.functionMap[call.name](call.args);
+    }
+  }
+  else {
+    Main.noFunction();
+  }
 }
 
-pipeline();
-
+document.getElementById("main").addEventListener('submit', (e) => {
+  e.target.value.preventDefault(); 
+  pipeline(document.getElementbyId('main').value); 
+})
