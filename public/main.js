1b056c233e8e3895811e99dd4501489e046f7b6e
mehta302 <mehta302@purdue.edu>
2024-08-11 21:14:37 -0400

    Styling + Filtering

--- public/main.js
+++ public/main.js
@@ -4,41 +4,48 @@ class Main {
   constructor() {}
 
   static functionMap = {
-    addTag: ({selection, tag, attributes,/*styles,*/ text}) => {
+    addTag: ({selection, tag, attributes, styles, text}) => {
       const element = d3.selectAll(selection).append(tag);
 
       for (const attribute of attributes) {
         element.attr(attribute.name, attribute.value);
       }
-      // for (const style of styles) {
-      //   element.style(style, styles[style]);
-      // }
+
+      for (const style of styles) {
+        element.style(style.name, styles.value);
+      }
 
       if (text) {
         element.text(text);
       }
     },
 
-    alterTag: ({selection, /*attributes, styles,*/ text}) => {
+    alterText: ({selection, text}) => {
       const element = d3.selectAll(selection);
-
-      // console.log(attributes);
-
-      // for (const attribute of attributes) {
-      //   element.attr(attribute, attributes[attribute]);
-      // }
-
-      // for (const style of styles) {
-      //   element.style(style, styles[style]);
-      // }
 
       if (text) {
         element.text(text);
+      }
+    },
+
+    alterAttribute: ({selection, attributes}) => {
+      const element = d3.selectAll(selection);
+
+      for (const attribute of attributes) {
+        element.attr(attribute.name, attribute.value);
+      }
+    },
+
+    alterStyle: ({selection, styles}) => {
+      const element = d3.selectAll(selection);
+
+      for (const style of styles) {
+        element.style(style.name, style.value);
       }
     },
 
     deleteTag: ({selection}) => {
-      d3.selectAll(selection).filter(".main").remove();
+      d3.selectAll(selection).filter(":not(.main)").remove();
     }
   };
 
@@ -59,49 +66,57 @@ class Main {
           },
           attributes: {
             type: "ARRAY",
-            description: "A list objects for HTML tag attributes and their specified values",
+            description: "Array of objects for HTML tag attributes and their specified values",
             items: {
-              description: "object specifying an HTML tag attribute and its value eg {name: 'id', value: 'one'}",
+              description: "Object specifying an HTML tag attribute and its value eg {name: 'id', value: 'one'}",
               type: "OBJECT",
               properties: {
                 name: {
                   type: "STRING",
-                  description: "name of the HTML attribute eg id"
+                  description: "Name of the HTML attribute eg id"
                 },
                 value: {
                   type: "STRING",
-                  description: "value of the corresponding HTML attribute"
+                  description: "Value of the corresponding HTML attribute"
                 }
               }
             }
           },
-          /*
           styles: {
             type: "ARRAY",
-            description: "A list of JSON objects of tag styles and their specified values eg '{ color: \"red\" }'"
+            description: "Array of objects for HTML tag styles and their specified values",
+            items: {
+              description: "Object specifying a CSS style name and its value eg {name: 'color', value: 'red'}",
+              type: "OBJECT",
+              properties: {
+                name: {
+                  type: "STRING",
+                  description: "Name of the CSS style eg color"
+                },
+                value: {
+                  type: "STRING",
+                  description: "Value of the corresponding CSS style"
+                }
+              }
+            }
           },
-          */
           text: {
             type: "STRING",
             description: "The text to be inserted into the newly generated tag"
           }
         },
-        required: ["selection", "tag", "attributes", /*"styles",*/ "text"]
+        required: ["selection", "tag", "attributes", "styles", "text"]
       }
     },
     {
-      name: "alterTag",
+      name: "alterText",
       parameters: {
         type: "OBJECT",
-        description: "Select HTML tag or tags based on a CSS selector, alter attributes, alter inline styles, and alter text",
+        description: "Select HTML tag or tags based on a CSS selector and alter text",
         properties: {
           selection: {
             type: "STRING",
-            description: "CSS selector to select elements to alter. For single selection use id eg #name, group selection use class eg .name and for all tags use just name of tag eg div"
-          },
-          tag: {
-            type: "STRING",
-            description: "Name of HTML tag to append eg div"
+            description: "CSS selector to select elements to alter. For single selection use id eg #name, group selection use class eg .name and for all tags use name of tag eg div"
           },
           // attributes: {
           //   type: "ARRAY",
@@ -130,7 +145,7 @@ class Main {
             description: "The text to replace the selected tag current text"
           }
         },
-        required: ["selection", "tag", /*"attributes", "styles",*/ "text"]
+        required: ["selection", "text"]
       }
     },
     {
